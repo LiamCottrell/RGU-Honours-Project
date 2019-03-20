e2520e18da5ccbf058ce4e4b6a43d5c7ec2bfd2b
Liam Cottrell <liamcottrell@hotmail.co.uk>
2019-03-20 18:48:09 +0000

    Chunky commit :(
    Re-implemented project using yarn as opposed to npm. React templating now started to be implemented, now abstracted the different a-frame components as jsx elements. Globe now has object mapped to the surface after bug fix where the boat was not being mapped correctly to the globe due to a difference in radius mapping.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
+	
+
+class App extends React.Component {
   render() {
     return (
-      <div>
-      </div>
-    );
+    	<a-scene ar stats >
+    		<Earth />
+    		<Camera />
+		</a-scene>
+	)
   }
 }
+
+
+class Boat extends React.Component {
+	render() {
+		return (
+			<a-entity a-location="lat:57.1186191; lon:-2.1397003393363447; radius:0.1; mode:relative; elevation:0">
+				<a-entity rotation="-90 0 0">
+					<a-gltf-model scale="0.00001 0.00001 0.00001" src="boat.glb">
+					</a-gltf-model>
+				</a-entity>
+			</a-entity>
+		)
+	}
+}
+
+
+
+
+class Earth extends React.Component{
+	render() {
+		return ( 
+			<a-entity id="world" position="0 0 -0.4" visible="true" a-terrain="
+	                observer:camera;
+	                radius:0.1;
+	                observer:camera;
+	            ">
+	            <Boat />
+			</a-entity>
+		)
+	}
+}
+
+
+class Camera extends React.Component{
+	render() {
+		return ( 
+    		<a-entity id="cameraRig">
+				<a-entity
+				    id="camera"
+				    camera="fov: 45; near:5; far:10"
+				    position="0 0 0"
+				    orbit-controls="
+				        target: #world;
+				        minDistance:1000;
+				        maxDistance:5000;
+				        minPolarAngle:0.1;
+				        maxPolarAngle:3.04159265359;
+				        enableProportionalVelocity:true;
+				    ">
+				</a-entity>
+			</a-entity>
+		)
+	}
+}
+
 
 export default App;
