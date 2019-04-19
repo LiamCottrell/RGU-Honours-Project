a384de3d70919364a84555ff60da1d7e0142f01f
Liam Cottrell <liamcottrell@hotmail.co.uk>
2019-04-19 15:46:04 +0100

    Added React Router
    Added React Router and configured separate Home and world pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import  { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './App';
+import Home from './Home';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const routes = (
+    <BrowserRouter>
+        <Route path="/" component={Home} exact={true}/>
+        <Route path="/world" component={App}/>
+    </BrowserRouter>
+);
+ReactDOM.render(routes, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
