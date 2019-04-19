8063180413d8392d601365c3dc0f264d0d20ffae
Liam Cottrell <liamcottrell@hotmail.co.uk>
2019-04-19 17:27:07 +0100

    Early Form search MMSI
    Early stages of form layout that will allow users to send get request to world page.
    Removal of client side data management planned to remove at a later date.

--- public/js/sortJSON.js
+++ public/js/sortJSON.js
@@ -3,20 +3,20 @@ $.getJSON("../data/export.10000.json", function(data) {
         return o.RecvTime.$date;
     });
     const id = _.where(sorted, { MMSI: 992351085 });
-    console.log(sorted);
-    console.log(id);
+    // console.log(sorted);
+    // console.log(id);
 
     const unique = _.countBy(sorted, function (o) {
         return o.MMSI
     });
-    console.log(unique);
+    // console.log(unique);
 
     const result = _.reduce(sorted, function(prev, curr) {
         let found = _.find(prev, function(el) { return el.RecvTime.$date === curr.RecvTime.$date; });
         found ? (found.total += curr.total) : prev.push(_.clone(curr));
         return prev;
     }, []);
-    console.log(result);
+    // console.log(result);
 });
 
 
