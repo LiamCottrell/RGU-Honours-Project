cc24729b52f0d0c20da42a301349b2613d4b50d5
Liam Cottrell <liamcottrell@hotmail.co.uk>
2019-04-21 11:12:26 +0100

    Multiple Ships Demo w/Traces
    20 random ships will now be chosen from the dataset to be shown to the user if no MMSI value is given in the queryString.

--- client/src/App.js
+++ client/src/App.js
@@ -8,41 +8,99 @@ const randomColor = require('randomcolor'); // import the script
 
 class App extends React.Component {
     // Initialize the state
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            id: null,
-            data: {}
+            ships: [],
+            data: {},
+            tempData: {}
         }
     }
+
     componentDidMount() {
         const values = queryString.parse(this.props.location.search);
+
         if (values.MMSI) {
             this.getDataMMSI(values.MMSI);
+        } else {
+            this.getRandomShips();
+
         }
 
 
     }
-    getDataMMSI = (id) => {
+
+    getDataMMSI = (mmsi) => {
         fetch('/api/getData')
             .then(res => res.json())
-            .then( data => _.where(data, { MMSI: parseInt(id, 10) } ))
-            .then(data => this.setState({ id , data }))
+            .then(data => _.where(data, {MMSI: parseInt(mmsi, 10)}))
+            .then(data => this.setState({data}))
+            .then(data => this.getShips(this.state.data))
+            .then(ships => this.setState({ships}))
+
+    };
+
+    getRandomShips = () => {
+        fetch('/api/getData')
+            .then(res => res.json())
+            .then(tempData => this.setState({tempData}))
+            .then(data => this.chooseRandom(this.state.tempData, 20))
+            .then(ships => this.setState({ships}))
+            .then(data => this.getDataFromShip(this.state.tempData, this.state.ships))
+            .then(data => this.setState({data}))
+    };
+
+    getDataFromShip = (data, ships) => {
+        let tempObjs = [];
+        ships.forEach(function (ship) {
+            let result = _.where(data, {MMSI: parseInt(ship, 10)});
+            result.forEach(function (allShip) {
+                tempObjs.push(allShip);
+            });
+            // tempObjs.push(result);
+        });
+        console.log('tempobj');
+        console.log(tempObjs);
+        return tempObjs;
+    };
+
+
+    getShips = (data) => {
+        console.log(data);
+        let items = _.countBy(data, function (o) {
+            return o.MMSI
+        });
+        console.log(items);
+        return Object.keys(items).map(function (index) {
+            return index;
+        });
+    };
+
+
+    chooseRandom = (data, quantity) => {
+        console.log(data);
+        let items = _.countBy(data, function (o) {
+            return o.MMSI
+        });
+
+        return _.sample(Object.keys(items).map(function (index) {
+            return index;
+        }), quantity);
     };
 
     render() {
-        let { data }  = this.state;
-        console.log(data);
+        let {data} = this.state;
+        let {ships} = this.state;
         return (
-            <a-scene ar stats >
-                { data ? (
-                    <Earth data = { data } />
+            <a-scene ar stats>
+                {data ? (
+                    <Earth ships={ships} data={data}/>
                 ) : (
-                    <Earth />
-                    )
+                    <Earth/>
+                )
                 }
-                <Popup />
-                <Camera />
+                <Popup/>
+                <Camera/>
             </a-scene>
         )
     }
@@ -53,12 +111,12 @@ class EarthObj extends React.Component {
     render() {
         let myLat = null;
         let myLon = null;
-        if (!this.props.Lat){
+        if (!this.props.Lat) {
             myLat = 60.27432;
         } else {
             myLat = this.props.Lat
         }
-        if (!this.props.Lon){
+        if (!this.props.Lon) {
             myLon = -1.08595;
         } else {
             myLon = this.props.Lon;
@@ -67,18 +125,18 @@ class EarthObj extends React.Component {
         return (
             <a-entity a-location={
                 'lat:' + myLat + ';'
-                +'lon:' + myLon + ';'
-                +'radius:0.15;'
-                +'mode:relative;'
-                +'elevation:0'}
+                + 'lon:' + myLon + ';'
+                + 'radius:0.15;'
+                + 'mode:relative;'
+                + 'elevation:0'}
             >
                 {
                     isBoat ? (
-                            <Boat data={this.props.data} />
+                            <Boat data={this.props.data}/>
                         )
                         :
                         (
-                            <Trace traceColour={this.props.traceColour} />
+                            <Trace traceColour={this.props.traceColour}/>
                         )
                 }
 
@@ -87,10 +145,12 @@ class EarthObj extends React.Component {
     }
 }
 
-class Earth extends React.Component{
+class Earth extends React.Component {
     render() {
         let boatPos = this.props.data;
-        let traceColour = randomColor();
+        let ships = this.props.ships;
+
+        console.log(Object.keys(boatPos).map);
 
         return (
             <a-entity id="world" position="0 0 -0.4" visible="true" a-terrain="
@@ -98,43 +158,54 @@ class Earth extends React.Component{
 	                radius:0.15;
 	                observer:camera;
 	            ">
-                { this.props.data ? (
-                    Object.keys(boatPos).map(function(name, index){
-                        if (index !== (Object.values(boatPos).length - 1)){
-                            //Return Boat Traces
-                            return (<EarthObj
-                                        key={name}
-                                        boat={false}
-                                        traceColour={traceColour}
-                                        Lat={Object.values(boatPos)[index].Latitude}
-                                        Lon={Object.values(boatPos)[index].Longitude}
-                                    />)
-                        } else {
-                            //Return Boat Model Object
-                            return (<EarthObj
-                                        key={name}
-                                        boat={true}
-                                        data={Object.values(boatPos)[index]}
-                                        Lat={Object.values(boatPos)[index].Latitude}
-                                        Lon={Object.values(boatPos)[index].Longitude}
-                                    />)
-                        }
-                    })
-                ) : (
-                    null
-                )
+
+                {
+                    // IF i have data
+                    this.props.data ? (
+                        //Take for every value i have in the ship MMSI array
+                        ships.map(function (mmsi) {
+                            let traceColour = randomColor();
+                            // Create local object where Boats in data have the provided MMSI
+                            let singleShip = _.where(boatPos, {MMSI: parseInt(mmsi, 10)});
+                            return (
+                                Object.keys(singleShip).map(function (index) {
+                                    if ((Object.keys(singleShip).length - 1) !== parseInt(index)) {
+                                        //Return Boat Traces
+                                        return (<EarthObj
+                                            key={index + mmsi}
+                                            boat={false}
+                                            traceColour={traceColour}
+                                            Lat={Object.values(singleShip)[index].Latitude}
+                                            Lon={Object.values(singleShip)[index].Longitude}
+                                        />)
+                                    } else {
+                                        //Return Boat Model Object
+                                        return (<EarthObj
+                                            key={index + mmsi}
+                                            boat={true}
+                                            data={Object.values(singleShip)[index]}
+                                            Lat={Object.values(singleShip)[index].Latitude}
+                                            Lon={Object.values(singleShip)[index].Longitude}
+                                        />)
+                                    }
+                                })
+                            )
+                        })
+                    ) : (
+                        null
+                    )
+
                 }
+
             </a-entity>
         )
     }
 }
 
-class Boat extends React.Component{
+class Boat extends React.Component {
     render() {
-        console.log('this is the dom data');
-        console.log(this.props.data);
         return (
-            <a-entity  rotation="-90 0 0">
+            <a-entity rotation="-90 0 0">
                 <a-gltf-model
                     data-mmsi={this.props.data.MMSI}
                     data-recvtime={this.props.data.RecvTime.$date}
@@ -145,13 +216,12 @@ class Boat extends React.Component{
                     scale="0.00002 0.00002 0.00002"
                     src="boat.glb"
                 />
-                {/*<a-animation begin="click" attribute="rotation" to="0 0 360" easing="linear" dur="2000" fill="backwards"/>*/}
             </a-entity>
         )
     }
 }
 
-class Trace extends React.Component{
+class Trace extends React.Component {
     render() {
         let traceColour = this.props.traceColour;
         return (
@@ -161,7 +231,7 @@ class Trace extends React.Component{
 }
 
 
-class Popup extends React.Component{
+class Popup extends React.Component {
 
     render() {
         return (
@@ -170,16 +240,18 @@ class Popup extends React.Component{
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 className="modal-title" id="exampleModalLabel">Details about this Ship.</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div id="shipContent" className="modal-body">
-                           testicle
+                            You shouldn't see this...
                         </div>
                         <div className="modal-footer">
-                            <button id="closePopup" type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button id="closePopup" type="button" className="btn btn-secondary"
+                                    data-dismiss="modal">Close
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -189,8 +261,7 @@ class Popup extends React.Component{
 }
 
 
-
-class Camera extends React.Component{
+class Camera extends React.Component {
     render() {
         return (
             <a-entity id="cameraRig">
@@ -210,8 +281,10 @@ class Camera extends React.Component{
                               geometry="primitive: ring; radiusInner: 0.0005; radiusOuter: 0.001;"
                               material="color: cyan; shader: flat"
                               cursor="maxDistance: 30; fuse: true">
-                        <a-animation begin="click" easing="ease-in" attribute="scale" fill="forwards" from="0.2 0.2 0.2" to="1 1 1" dur="150"/>
-                        <a-animation begin="fusing" easing="ease-in" attribute="scale" fill="backwards" from="1 1 1" to="0.2 0.2 0.2" dur="1500"/>
+                        <a-animation begin="click" easing="ease-in" attribute="scale" fill="forwards" from="0.2 0.2 0.2"
+                                     to="1 1 1" dur="150"/>
+                        <a-animation begin="fusing" easing="ease-in" attribute="scale" fill="backwards" from="1 1 1"
+                                     to="0.2 0.2 0.2" dur="1500"/>
                     </a-entity>
                 </a-entity>
             </a-entity>
